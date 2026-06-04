26f8804f22de81bd22fa81d39cd52d8b4b70645a
luis <luisgb.jaramillo@gmail.com>
2026-06-03 19:48:21 -0500

    Esta es la instancia

--- src/main/java/co/edu/uniremignton/Main.java
+++ src/main/java/co/edu/uniremignton/Main.java
@@ -1,16 +1,19 @@
-package co.edu.uniremignton;
-
-import co.edu.uniremignton.service.Library;
-
 /*
-⏺PUNTO DE ENTRADA DE LA APLICACION, DONDE SE INICIA,
+⏺️PUNTO DE ENTRADA DE LA APLICACION, DONDE SE INICIA,
 EN EL MAIN CREAMOS UNA INSTANCIA DE LA BIBLIOTECA,
 Y DESDE AQUI SE PODRA INICIAR LA INTERFAZ DE USUARIO
 PARA REGISTRAR LIBROS O REALIZAR OTRAS OPERACIONES.
 */
 
+package co.edu.uniremignton;
+import co.edu.uniremignton.service.Library;
+import co.edu.uniremignton.ui.ConsoleUI;
+
 public class Main {
     public static void main(String[] args) {
-        Library library = new Library(); // ➡CREAMOS UNA INSTANCIA DE LA BIBLIOTECA
-    }
+        Library library = new Library();
+        ConsoleUI consoleUI = new ConsoleUI(library);
+
+        // PRENDEMOS LA ConsoleUI
+        consoleUI.start();    }
 }
